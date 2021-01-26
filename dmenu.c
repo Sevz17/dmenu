5c60fbdd287cedfea8d50e4dbfd2ac6cf43c6af3
Sevz17 <leohdz172@outlook.com>
2021-01-26 12:56:06 -0600

    Keybindings: changed keybindings for Tab, Ctrl+Tab and Ctrl+i, now Ctrl+v paste selection

--- dmenu.c
+++ dmenu.c
@@ -352,12 +352,20 @@ keypress(XKeyEvent *ev)
 		case XK_f: ksym = XK_Right;     break;
 		case XK_g: ksym = XK_Escape;    break;
 		case XK_h: ksym = XK_BackSpace; break;
-		case XK_i: ksym = XK_Tab;       break;
+		case XK_i:
+			if (!sel)
+				return;
+			strncpy(text, sel->text, sizeof text - 1);
+			text[sizeof text - 1] = '\0';
+			cursor = strlen(text);
+			match();
+			break;
 		case XK_j: /* fallthrough */
 		case XK_J: /* fallthrough */
 		case XK_m: /* fallthrough */
 		case XK_M: ksym = XK_Return; ev->state &= ~ControlMask; break;
 		case XK_n: ksym = XK_Down;      break;
+		case XK_Tab: /* fallthrough */
 		case XK_p: ksym = XK_Up;        break;
 
 		case XK_k: /* delete right */
@@ -374,6 +382,7 @@ keypress(XKeyEvent *ev)
 				insert(NULL, nextrune(-1) - cursor);
 			break;
 		case XK_y: /* paste selection */
+		case XK_v:
 		case XK_Y:
 			XConvertSelection(dpy, (ev->state & ShiftMask) ? clip : XA_PRIMARY,
 			                  utf8, utf8, win, CurrentTime);
@@ -514,19 +523,12 @@ insert:
 		if (lines > 0)
 			return;
 		/* fallthrough */
+	case XK_Tab: /* fallthrough */
 	case XK_Down:
 		if (sel && sel->right && (sel = sel->right) == next) {
 			curr = next;
 			calcoffsets();
 		}
-		break;
-	case XK_Tab:
-		if (!sel)
-			return;
-		strncpy(text, sel->text, sizeof text - 1);
-		text[sizeof text - 1] = '\0';
-		cursor = strlen(text);
-		match();
 		break;
 	}
 
